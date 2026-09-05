a7ca46ae482b306b687267da2f371da83ea8ac69
Truc <icloud07072010@gmail.com>
2026-09-05 10:02:01 +0700

    ..

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCR2lbKd2TEPl-01VObuV7eWfcSkpacfZA",
+  authDomain: "englishproject-c0131.firebaseapp.com",
+  projectId: "englishproject-c0131",
+  storageBucket: "englishproject-c0131.firebasestorage.app",
+  messagingSenderId: "270223735960",
+  appId: "1:270223735960:web:6d4aa35c04993b0f7724ed",
+  measurementId: "G-GD5P6V42DW"
+};
